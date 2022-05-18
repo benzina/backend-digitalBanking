bc01e1e516fd2d405156882658d38453cd74b8ac
EL MAHDI BENZINA <benzina.elmahdi@gmail.com>
2022-05-18 17:17:21 +0100

    initial commit : create entities and generate database

---
+++ src/main/java/com/example/demo/entities/BankAccount.java
@@ -0,0 +1,26 @@
+package com.example.demo.entities;
+
+import com.example.demo.enums.AccountStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "bankAccountType",length = 2,discriminatorType = DiscriminatorType.STRING)
+@Data @NoArgsConstructor @AllArgsConstructor
+public class BankAccount {
+    @Id
+    private String id;
+    private Date createdAt;
+    private double balance;
+    private AccountStatus status;
+    private String currency;
+    @ManyToOne
+    private Customer customer;
+    @OneToMany(mappedBy = "bankAccount")
+    private List<Operation> operations;
+}
---
+++ src/main/java/com/example/demo/entities/Operation.java
@@ -0,0 +1,21 @@
+package com.example.demo.entities;
+
+import com.example.demo.enums.OperationType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor
+public class Operation {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Date date;
+    private double amount;
+    private OperationType type;
+    @ManyToOne
+    private BankAccount bankAccount;
+
+}
